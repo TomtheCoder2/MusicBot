b6c077f19cc5566e7481b605b78355c6c6307c60
TomtheCoder2 <57863175+TomtheCoder2@users.noreply.github.com>
2021-09-19 16:52:34 +0200

    some changes

---
+++ src/main/java/com/github/TomtheCoder2/musicbot/Command.java
@@ -0,0 +1,10 @@
+package com.github.TomtheCoder2.musicbot;
+
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import reactor.core.publisher.Mono;
+
+interface Command {
+    // Since we are expecting to do reactive things in this method, like
+    // send a message, then this method will also return a reactive type.
+    Mono<Void> execute(MessageCreateEvent event);
+}
